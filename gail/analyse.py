d481b40fee10c14a977cc444dd04da78e2930c23
wdwd12345 <fdjwd@126.com>
2020-11-10 15:47:43 +0800

    modify gail

--- gail/analyse.py
+++ gail/analyse.py
@@ -1,5 +1,16 @@
 import matplotlib.pyplot as plt 
 
+benchmark = {
+    "dark": 1314760.0,
+    "sebf": 1072680.0,
+    "fifo": 3963584.0
+}
+benchmark = {
+    "dark": 2.4247392E7,
+    "sebf": 1.5005968E7, 
+    "fifo": 4.3473352E7, 
+}
+
 def analyse_reward_list():
     file = "log/log.txt"
     with open(file, "r") as f:
@@ -23,11 +34,7 @@ def analyse_reward_list():
 
 def analyse_result_log():
     file = "doc/log/gail/2_log.txt"
-    benchmark = {
-        "dark": 1314760.0,
-        "sebf": 1072680.0,
-        "fifo": 3963584.0
-    }
+    file = "log/log.txt"
     with open(file, "r") as f:
         line = f.readline()
         results = []
@@ -44,9 +51,31 @@ def analyse_result_log():
         plt.legend(["GAIL", "Aalo", "SEBF", "FIFO"])
         plt.xlabel("Episode")
         plt.ylabel("Total CCT")
-        plt.show()
+
+def analyse_test_result_log():
+    file = "log/test_log.txt"
+    with open(file, "r") as f:
+        line = f.readline()
+        results = []
+        while line:
+            if line.startswith("Test/Result"):
+                result = eval(line.split(":")[-1])
+                if result > 0:
+                    results.append(result)
+            line = f.readline()
+        plt.figure()
+        plt.plot(results, ".-")
+        plt.plot([0, len(results)], [benchmark["dark"]]*2, "orange")
+        plt.plot([0, len(results)], [benchmark["sebf"]]*2, "r")
+        plt.plot([0, len(results)], [benchmark["fifo"]]*2, "cyan")
+        plt.legend(["GAIL", "Aalo", "SEBF", "FIFO"])
+        plt.xlabel("Episode")
+        plt.ylabel("Total CCT")
 
 if __name__ == "__main__":
     pass
     # analyse_reward_list()
     analyse_result_log()
+
+    analyse_test_result_log()
+    plt.show()
